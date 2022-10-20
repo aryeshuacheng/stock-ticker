dd64055859fe1c24c8dc75d9db5192176bfd1ec4
Andrew Cheng <andrew.cheng@visaops.com>
2022-10-20 16:01:15 -0400

    Added routes to buy and sell

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -7,34 +7,63 @@ class Api::V1::StocksController < ApplicationController
   end
 
   def get_quotes
-
-
     quotes = []
 
     @stocks = @player.portfolio.stocks
 
     @stocks.each do |stock|
-      quotes << StockQuote::Stock.quote(stock.symbol)
+      quotes << {api_data: StockQuote::Stock.quote(stock.symbol), shares: stock.shares}
     end
 
     if quotes
-      render json: {data: quotes}, status: :ok
+      render json: quotes, status: :ok
     else
-      render json: stock_quote.errors, status: :bad_request
+      render json: quotes.errors, status: :bad_request
     end
   end
 
   def add_stock_to_portfolio
-
     Stock.find_or_create_by(symbol: (params[:symbol]), portfolio_id: @player.portfolio.id, shares: 0)
   end
 
   def buy_stock
+    price_quote = StockQuote::Stock.quote(player_params['symbol'])
+    cost_of_purchase = price_quote.latest_price * 1
 
+    if @player.available_cash > cost_of_purchase
+      @player.update(available_cash: @player.available_cash - cost_of_purchase)
+
+      stock = Stock.where(portfolio_id: @portfolio.id, symbol: player_params['symbol']).first
+      if stock.present?
+        updated_quantity = stock.shares + 1
+        stock.update(shares: updated_quantity)
+      end
+
+      render json: @player.available_cash, status: :ok
+    else
+      render json: {message: 'Insufficient funds!'}, status: :ok
+    end
   end
 
   def sell_stock
+    price_quote = StockQuote::Stock.quote(player_params['symbol'])
+    cost_of_sale = price_quote.latest_price * 1
 
+    stock = Stock.where(portfolio_id: @portfolio.id, symbol: player_params['symbol']).first
+
+    if stock.present?
+      @player.update(available_cash: @player.available_cash + cost_of_sale)
+
+      stock = Stock.where(portfolio_id: @portfolio.id, symbol: player_params['symbol']).first
+      if stock.present?
+        updated_quantity = stock.shares - 1
+        stock.update(shares: updated_quantity)
+      end
+
+      render json: @player.available_cash, status: :ok
+    else
+      render json: {message: 'Cannot sell more shares than you own!'}, status: :ok
+    end
   end
 
   def available_cash
@@ -44,6 +73,6 @@ class Api::V1::StocksController < ApplicationController
   private
 
   def player_params
-    params.permit(:name,:symbol)
+    params.permit(:name,:symbol,:quantity_of_shares_to_purchase)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get 'get_quotes', to: 'stocks#get_quotes'
       get 'add_stock_to_portfolio', to: 'stocks#add_stock_to_portfolio'
       get 'available_cash', to: 'stocks#available_cash'
+      get 'buy_stock', to: 'stocks#buy_stock'
+      get 'sell_stock', to: 'stocks#sell_stock'
     end
   end
 end
